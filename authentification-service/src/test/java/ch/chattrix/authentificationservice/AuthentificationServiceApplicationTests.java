c37ace021e5a240684103f081e19912ab653afe2
loris-sigrist <186710904+loris-sigrist@users.noreply.github.com>
2026-06-11 20:16:33 +0200

    add authentification-service springboot-project

---
+++ authentification-service/src/test/java/ch/chattrix/authentificationservice/AuthentificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.chattrix.authentificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthentificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
